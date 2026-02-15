1966c83fdb4de8928e4abb47033da69656ee6f67
harriskrem <harriskremm@outlook.com>
2026-02-15 16:58:20 +0200

    chore(deps): migrate tailwind to v4

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,5 @@
-import tailwindcss from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
-
 export default {
-  tailwindcss: {},
-  autoprefixer: {},
-  plugins: [tailwindcss, autoprefixer]
+  plugins: {
+    '@tailwindcss/postcss': {},
+  },
 }
